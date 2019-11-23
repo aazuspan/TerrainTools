Fix off by one indexing error and rescale hillshade values

TerrainOutput.py
@@ -82,9 +82,10 @@ class Slope(TerrainOutput):
         # The slope array must be smaller than the DEM to avoid interpolating border data
         slope_array = np.empty((self.dem.shape[0] - 2, self.dem.shape[1] - 2), dtype=float)
 
-        for row in range(1, self.dem.shape[0] - 2):
-            for col in range(1, self.dem.shape[1] - 2):
-                z = self.get_neighbours(row, col)
+        for row in range(slope_array.shape[0]):
+            for col in range(slope_array.shape[1]):
+                # Slope array is inset 1 pixel from the DEM
+                z = self.get_neighbours(row + 1, col + 1)
                 slope = None
 
                 # TODO: Confirm that the index transformations are accurate
@@ -140,9 +141,10 @@ class Aspect(TerrainOutput):
         # The aspect array must be smaller than the DEM to avoid interpolating border data
         aspect_array = np.empty((self.dem.shape[0] - 2, self.dem.shape[1] - 2), dtype=float)
 
-        for row in range(1, self.dem.shape[0] - 2):
-            for col in range(1, self.dem.shape[1] - 2):
-                z = self.get_neighbours(row, col)
+        for row in range(aspect_array.shape[0]):
+            for col in range(aspect_array.shape[1]):
+                # Aspect array is inset 1 pixel from the DEM
+                z = self.get_neighbours(row + 1, col + 1)
 
                 # See http://desktop.arcgis.com/en/arcmap/10.3/tools/spatial-analyst-toolbox/how-aspect-works.htm
                 slope_ew = ((z[2] + 2 * z[3] + z[4]) - (z[0] + 2 * z[7] + z[6])) / (8 * self.cell_resolution)
@@ -172,7 +174,7 @@ class Hillshade(TerrainOutput):
         super().__init__(dem, cell_resolution)
 
     def generate(self):
-        # The aspect array must be smaller than the DEM to avoid interpolating border data
+        # The hillshade array must be smaller than the DEM to avoid interpolating border data
         hillshade_array = np.empty((self.dem.shape[0] - 2, self.dem.shape[1] - 2), dtype=float)
 
         # Convert light source altitude in degrees to zenith in radians
@@ -183,18 +185,22 @@ class Hillshade(TerrainOutput):
         if azimuth_rad > 2 * math.pi:
             azimuth_rad -= 2 * math.pi
 
-        for row in range(1, self.dem.shape[0] - 2):
-            for col in range(1, self.dem.shape[1] - 2):
+        for row in range(hillshade_array.shape[0]):
+            for col in range(hillshade_array.shape[1]):
                 slope_rad = math.radians(self.slope[row][col])
                 # Hillshade requires 0 to be East instead of North, so subtract 180
                 aspect_rad = math.radians(self.aspect[row][col] - 180)
 
                 # See https://pro.arcgis.com/en/pro-app/tool-reference/3d-analyst/how-hillshade-works.htm
-                hillshade = 255 * ((math.cos(zenith_rad) * math.cos(slope_rad) + (math.sin(zenith_rad) * math.sin(slope_rad) * math.cos(azimuth_rad - aspect_rad))))
+                hillshade = 255 * (math.cos(zenith_rad) * math.cos(slope_rad) +
+                                   (math.sin(zenith_rad) * math.sin(slope_rad) * math.cos(azimuth_rad - aspect_rad)))
 
                 if hillshade < 0:
                     hillshade = 0
 
                 hillshade_array[row][col] = hillshade
+
+        # Rescale the hillshade values from 0 to 255
+        hillshade_array = np.interp(hillshade_array, (np.min(hillshade_array), np.max(hillshade_array)), (0, 255))
 
         return hillshade_array