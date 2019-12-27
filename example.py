8059ebcc88ad54bc01a1f21f156fbf2b78ffeb96
azuspan607 <aa.zuspan@gmail.com>
2019-12-27 11:17:47 -0800

    Update examples and add example data

--- example.py
+++ example.py
@@ -2,15 +2,25 @@ import matplotlib.pyplot as plt
 from Terrain import Terrain
 from Constants import SlopeAlgorithms, SlopeUnits
 
+
 if __name__ == "__main__":
-    terrain = Terrain(dem="example_data\\black_canyon_dem_small_16bit.png", cell_resolution=30)
-    # aspect = terrain.calculate_aspect()
-    # slope = terrain.calculate_slope(algorithm=SlopeAlgorithms.NEIGHBORHOOD, units=SlopeUnits.DEGREES)
-    hillshade = terrain.calculate_hillshade(azimuth=210)
-    hillshade.save("hillshade_210deg.png")
-    # aspect.save('aspect.tif')
-    # plt.imshow(slope.array)
-    # plt.imshow(aspect.array, cmap="Greys_r")
-    # plt.show()
+    # Create a terrain object from a Digital Elevation Model
+    terrain = Terrain(dem="example_data\\black_canyon_dem_small.png", cell_resolution=30)
+
+    # Calculate the terrain outputs from the input DEM
+    slope = terrain.calculate_slope(algorithm=SlopeAlgorithms.NEIGHBORHOOD, units=SlopeUnits.DEGREES)
+    aspect = terrain.calculate_aspect()
+    hillshade = terrain.calculate_hillshade(azimuth=315)
+
+    # Preview the terrain outputs using pyplot
+    plt.imshow(slope.array)
+    plt.show()
+    plt.imshow(aspect.array)
+    plt.show()
     plt.imshow(hillshade.array, cmap="Greys_r")
     plt.show()
+
+    # Save the terrain outputs as local files
+    slope.save("example_data\\output\\slope.png")
+    aspect.save("example_data\\output\\aspect.png")
+    hillshade.save("example_data\\output\\hillshade.png")
